832d9118a64a4985b0cdf57b58c5edcf0d763e87
Anson <ansonlau39@gmail.com>
2022-01-05 09:44:41 -0800

    styled footer and added custom button component

--- src/components/footer/footer.component.jsx
+++ src/components/footer/footer.component.jsx
@@ -1,23 +1,25 @@
 import React from "react";
 
-import "./footer.styles.scss"
+import "./footer.styles.scss";
 
 const Footer = () => (
-    <div className="footer">
-        <div className="footnote">Clear Web Ltd.</div>
-        <h1>Where functionality meets the aesthetic</h1>
-        <div className="right-side">
-            <span className="footer-links">Services</span>
-            <span className="footer-links">Pricing</span>
-            <span className="footer-links">Portfolio</span>
-            <div className="socials">
-                <span className="social-logos">FB</span>
-                <span className="social-logos">IG</span>
-                <span className="social-logos">Twitter</span>
-                <span className="social-logos">YT</span>
-            </div>
-        </div>
+  <div className="footer">
+    <div className="left-side">
+      <div className="footnote">Clear Web Ltd.</div>
+      <h1>Where functionality meets the aesthetic</h1>
     </div>
-)
+    <div className="right-side">
+      <span className="footer-links">Services</span>
+      <span className="footer-links">Pricing</span>
+      <span className="footer-links">Portfolio</span>
+      <div className="socials">
+        <span className="social-logos">FB</span>
+        <span className="social-logos">IG</span>
+        <span className="social-logos">Twitter</span>
+        <span className="social-logos">YT</span>
+      </div>
+    </div>
+  </div>
+);
 
-export default Footer
+export default Footer;
--- src/pages/homepage/homepage.component.jsx
+++ src/pages/homepage/homepage.component.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import Banner from "../../components/banner/banner.component";
+import CustomButton from "../../components/custom-button/custom-button.component";
 import Footer from "../../components/footer/footer.component";
 
 import "./homepage.styles.scss";
@@ -25,23 +26,28 @@ const Homepage = () => (
         </div>
         <h4 className="content">2. Commitment to meeting your deadlines</h4>
         <div className="content">
-        Our team will work 7 days per week (excluding major holidays) to make sure your needs are met within a reasonable timeline.
+          Our team will work 7 days per week (excluding major holidays) to make
+          sure your needs are met within a reasonable timeline.
         </div>
         <h4 className="content">3. Flexible development protocol</h4>
         <div className="content">
-        No one business is the same. Our team will choose an effective development strategy to match your specific business needs.
+          No one business is the same. Our team will choose an effective
+          development strategy to match your specific business needs.
         </div>
         <h4 className="content">4. Results driven</h4>
         <div className="content">
-        Our team is focused on developing a website that drives new customers. leads, and sales.
+          Our team is focused on developing a website that drives new customers.
+          leads, and sales.
         </div>
       </div>
     </div>
     <div className="portfolio-section">
-        <h1 className="content">See Our Recent Work</h1>
-        <div className="content">Portfolio</div>
+      <h1 className="content">See Our Recent Work</h1>
+      <div className="content">
+        <CustomButton>Portfolio</CustomButton>
+      </div>
     </div>
-    <Banner/>
+    <Banner />
     <Footer />
   </div>
 );
