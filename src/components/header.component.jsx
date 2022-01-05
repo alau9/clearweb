f6206edb1857314435c25cc2f9686fbe43efa20e
Anson <ansonlau39@gmail.com>
2022-01-04 16:53:38 -0800

    initial commit

---
+++ src/components/header.component.jsx
@@ -0,0 +1,18 @@
+import React from "react";
+
+import "./header.styles.scss"
+
+const Header = () => (
+    <div className="header">
+         <span className="header-items left-side">LOGO</span>
+        <span className="header-items right-side">SERVICES</span>
+        <span className="header-items">PORTFOLIO</span>
+        <span className="header-items">ABOUT</span>
+        <span className="header-items">TWITTER</span>
+        <span className="header-items">IG</span>
+        <span className="header-items">YT</span>
+        <button>PRICING</button>
+    </div>
+)
+
+export default Header
